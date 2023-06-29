61ef3d93b1bacd26a0c5df398fccdc839534125e
71226_mahesh <maheshpy2508@gmail.com>
2023-06-29 17:15:32 +0530

    modified

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var exp=require('express')
-
+var fs=require('fs')
 var app=exp()
 
-app.listen(9000,function(){console.log("Server Started at 9000")})
-app.use(exp.static('public'))
 
-app.get('/gethtml',function(req,res){
- res.sendFile(__dirname+"/public/form.html")
-   })
+app.listen(9000,function(){console.log("Server Started at 9000")})
+
+app.use(express.static(path.join(__dirname,'public')));
+
+ app.get('/', function (req, res) {
+   res.sendFile(path.join(___dirname + '/form.html'));
+});
+ 
